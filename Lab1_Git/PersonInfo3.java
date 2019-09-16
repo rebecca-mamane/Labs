a7fddf5af3e64b075b7a9e57a49c6c78d7c127ab
notshravan <svyas050@uottawa.ca>
2019-09-16 19:43:28 -0400

    mon premier commit

--- Lab1_Git/PersonInfo3.java
+++ Lab1_Git/PersonInfo3.java
@@ -1,8 +1,8 @@
 public class PersonInfo3 {
 
-    public static String firstName = "Liz";
-    public static String lastName = "Grégoire";
-    public static int studentNo = 173819;
+    public static String firstName = "shravan";
+    public static String lastName = "Vyas";
+    public static int studentNo = 300089045;
     public static String description = "Likes long walks along the beach";
 
     public static void displayFr() {
