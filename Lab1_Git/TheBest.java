fb38374a2f4452e3ee0ef0738ddb8b87b0a2d5ed
Rébecca Mamane <rmama012@uottawa.ca>
2019-09-16 20:21:50 -0400

    mofied java code

--- Lab1_Git/TheBest.java
+++ Lab1_Git/TheBest.java
@@ -2,6 +2,13 @@
 public class TheBest {
     
     public static void main(String[] args) {
-
+		PersonInfo1 r = new PersonInfo1;
+		r.displayFR();
+		System.out.println();
+		PersonInfo1 s = new PersonInfo3;
+		s.displayFR();
+		System.out.println();
+		PersonInfo1 d = new PersonInfo1;
+		d.displayFR();
     }
 }
