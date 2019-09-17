a58520dee4ed444436af89b91c324e3c493f178c
Cheikh Bassirou Ndoye <cndoy096@uottawa.ca>
2019-09-16 20:20:45 -0400

    PersonInfo3

--- Lab1_Git/PersonInfo3.java
+++ Lab1_Git/PersonInfo3.java
@@ -1,20 +1,10 @@
 public class PersonInfo3 {
-
-<<<<<<< HEAD
     public static String firstName = "Cheikh Bassirou";
     public static String lastName = "Ndoye";
     public static int studentNo = 300045791;
     public static String description = "Walk it like I talk it";
-=======
-    public static String firstName = "shravan";
-    public static String lastName = "Vyas";
-    public static int studentNo = 300089045;
-<<<<<<< HEAD
-    public static String description = "Might as well Like a few long walks along the beach+ more description along the way .....";
-=======
-    public static String description = "Likes short runs along the beach";
->>>>>>> newBranch
->>>>>>> 27c67bb6a2825d2893f5cd125dd67687c2cead37
+
+   
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
